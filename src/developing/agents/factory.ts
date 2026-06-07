8c5a2029ad52d8683cbbb7ba4de13272b6d56e64
yindaheng98 <yindaheng98@gmail.com>
2026-06-06 20:09:26 -0700

    factory

--- src/developing/agents/factory.ts
+++ src/developing/agents/factory.ts
@@ -1,19 +1,11 @@
 import type { AgentFactoryMap } from "coding-agent-forge";
 
-import { CodingPlanInterpreterAgent } from "./interpreter.js";
 import { DeveloperAgent } from "./developer.js";
-import { HarnessEngineerAgent } from "./harness.js";
+import { OrchestratorAgent } from "./orchestrator.js";
 import { CodeReviewerAgent } from "./reviewer.js";
-import { ExperimentContractAuditorAgent } from "./auditor.js";
-import { IntegrationManagerAgent } from "./manager.js";
 
 export const agentFactories: AgentFactoryMap = {
-  "coding-plan-interpreter": (thread, constants) =>
-    new CodingPlanInterpreterAgent(thread, constants),
+  orchestrator: (thread, constants) => new OrchestratorAgent(thread, constants),
   developer: (thread, constants) => new DeveloperAgent(thread, constants),
-  "harness-engineer": (thread, constants) => new HarnessEngineerAgent(thread, constants),
   "code-reviewer": (thread, constants) => new CodeReviewerAgent(thread, constants),
-  "experiment-contract-auditor": (thread, constants) =>
-    new ExperimentContractAuditorAgent(thread, constants),
-  "integration-manager": (thread, constants) => new IntegrationManagerAgent(thread, constants),
 };
