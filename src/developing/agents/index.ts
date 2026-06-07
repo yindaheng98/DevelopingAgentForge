842dcd6e7cfd1aa1bfff77c4f3416680cc6b8a71
yindaheng98 <yindaheng98@gmail.com>
2026-06-06 20:56:27 -0700

    export

--- src/developing/agents/index.ts
+++ src/developing/agents/index.ts
@@ -1,10 +1,4 @@
-export { CodingPlanInterpreterAgent, type CodingPlanInterpreterVariables } from "./interpreter.js";
-export { DeveloperAgent, type DeveloperConstants, type DeveloperVariables } from "./developer.js";
-export { HarnessEngineerAgent, type HarnessEngineerVariables } from "./harness.js";
+export { CodingManagerAgent, type CodingManagerVariables } from "./manager.js";
+export { DeveloperAgent, type DeveloperVariables } from "./developer.js";
 export { CodeReviewerAgent, type CodeReviewerVariables } from "./reviewer.js";
-export {
-  ExperimentContractAuditorAgent,
-  type ExperimentContractAuditorVariables,
-} from "./auditor.js";
-export { IntegrationManagerAgent, type IntegrationManagerVariables } from "./manager.js";
 export { agentFactories } from "./factory.js";
