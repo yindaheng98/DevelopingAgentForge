9c35a74a7b81cd261fd858caee45e794f58bedf7
yindaheng98 <yindaheng98@gmail.com>
2026-06-15 17:38:37 -0700

    include trajectory-optimizer

--- src/agents/factory.ts
+++ src/agents/factory.ts
@@ -3,9 +3,11 @@ import type { AgentFactoryMap } from "coding-agent-forge";
 import { DeveloperAgent } from "./developer.js";
 import { CodingManagerAgent } from "./manager.js";
 import { CodeReviewerAgent } from "./reviewer.js";
+import { TrajectoryOptimizerAgent } from "./trajectory-optimizer.js";
 
 export const agentFactories: AgentFactoryMap = {
   "coding-manager": (thread, constants) => new CodingManagerAgent(thread, constants),
   developer: (thread, constants) => new DeveloperAgent(thread, constants),
   "code-reviewer": (thread, constants) => new CodeReviewerAgent(thread, constants),
+  "trajectory-optimizer": (thread, constants) => new TrajectoryOptimizerAgent(thread, constants),
 };
